24f36ac88116f087c0227a1380d41cc48a9e88a3
Yoojin-Lee <yjysfo@gmail.com>
2020-12-26 23:31:34 +0900

    week04_homework_salty_app

--- week04/app.py
+++ week04/app.py
@@ -8,7 +8,7 @@ client = MongoClient('localhost', 27017)
 db = client.dbhomework
 
 
-## HTML 화면 보기
+## HTML 화면 보여주기
 @app.route('/')
 def homework():
     return render_template('index.html')
@@ -17,29 +17,23 @@ def homework():
 # 주문하기(POST) API
 @app.route('/order', methods=['POST'])
 def save_order():
-    name_receive = request.form['name_give']
-    count_receive = request.form['count_give']
-    address_receive = request.form['address_give']
-    phone_receive = request.form['phone_give']
 
-    doc = {
-        'name': name_receive,
-        'count': count_receive,
-        'address': address_receive,
-        'phone': phone_receive
+    order = {
+        'name': request.form['name_give'],
+        'count': request.form['count_give'],
+        'address': request.form['address_give'],
+        'phone': request.form['phone_give'],
     }
-
-    db.homework.insert_one(doc)
-
+    db.orders.insert_one(order)
     return jsonify({'result': 'success', 'msg': '주문이 완료되었습니다.'})
 
 
 # 주문 목록보기(Read) API
 @app.route('/order', methods=['GET'])
 def view_orders():
-    orders = list(db.homework.find({}, {'_id': 0}))
+    orders = list(db.orders.find({}, {'_id': 0}))
     return jsonify({'result': 'success', 'orders': orders})
 
 
 if __name__ == '__main__':
-    app.run('127.0.0.1', port=5000, debug=True)
+    app.run('127.0.0.1', port=5001, debug=True)
